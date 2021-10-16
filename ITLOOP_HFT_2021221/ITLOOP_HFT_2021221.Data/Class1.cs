65b81cba0b9f877265b838323ce87e8268ae4bc9
danakhang <70797792+danakhang@users.noreply.github.com>
2021-10-16 21:33:09 +0200

    adding nuget packages, the mdf ang ldf files

--- ITLOOP_HFT_2021221/ITLOOP_HFT_2021221.Data/Class1.cs
+++ ITLOOP_HFT_2021221/ITLOOP_HFT_2021221.Data/Class1.cs
@@ -2,6 +2,7 @@
 
 namespace ITLOOP_HFT_2021221.Data
 {
+    // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=ITLOOP_HFT_2021221.Data\Database1.mdf;Integrated Security=True
     public class Class1
     {
     }
