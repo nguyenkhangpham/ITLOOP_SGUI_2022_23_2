953e8018624e91c55fa9b2f7c8457d52964dc886
danakhang <70797792+danakhang@users.noreply.github.com>
2021-10-18 22:24:54 +0200

    update

--- ITLOOP_HFT_2021221/ITLOOP_HFT_2021221.Data/DbContext.cs
+++ ITLOOP_HFT_2021221/ITLOOP_HFT_2021221.Data/DbContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ITLOOP_HFT_2021221.Data
 {
     class DbContext
     {
-        modelBuilder.Entity<Post>().OwnsOne(p => p.AuthorName).HasData(
+        ModelBuilder.Entity<Post>().OwnsOne(p => p.AuthorName).HasData(
         new { StudentId = 1, FirstName = "Andriy", LastName = "Svyryd" },
         new { StudentId = 2, FirstName = "Diego", LastName = "Vega" });
         using (var context = new DataSeedingContext())
