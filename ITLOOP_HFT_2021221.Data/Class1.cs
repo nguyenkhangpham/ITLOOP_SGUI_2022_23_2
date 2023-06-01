284fcb204b95c3340633391a906de00c2791e41b
Nguyen Khang Pham <70797792+nguyenkhangpham@users.noreply.github.com>
2023-06-01 04:19:11 +0200

    update

--- ITLOOP_HFT_2021221.Data/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ITLOOP_HFT_2021221.Data
-{
-    // @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True"
-    public class Class1
-    {
-    }
-}
